fff2832bcc05e9923a086e9ddf576a2e15351b88
Jenkins <jenkins@bqbot.bq.local>
2019-10-08 17:45:37 +0200

    Version 2.10.0_20190924-0755 released
    Based on TAG LA.UM.6.6.r1-11300-89xx.0 from CAF.

--- sound/soc/msm/qdsp6v2/q6afe.c
+++ sound/soc/msm/qdsp6v2/q6afe.c
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 /* Copyright (c) 2012-2017, 2019 The Linux Foundation. All rights reserved.
-=======
-/* Copyright (c) 2012-2019, The Linux Foundation. All rights reserved.
->>>>>>> LA.UM.6.6.r1-11300-89xx.0
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
